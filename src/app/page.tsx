f1ae86170b354f0e1ac3ed85138b0dac2a3118a1
Kristian <49764796+kristianka@users.noreply.github.com>
2024-06-28 14:13:46 +0300

    make site better on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import React from "react";
 export default function Home() {
     return (
         <main className="">
-            <div>
-                <div className="m-5 grid grid-cols-2">
+            <div className="m-5">
+                <div className="grid grid-cols-1 sm:grid-cols-2">
                     <AboutMe />
-                    <div className="m-auto">
+                    <div className="mt-3 m-auto">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae amet
                         ea, architecto eius nostrum et quia iusto labore deserunt, reprehenderit
                         officia harum inventore itaque! Possimus necessitatibus tenetur vero
