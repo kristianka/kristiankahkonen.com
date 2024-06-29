5690c905d5d4bcca0c77a1da4ee6acc67151fd69
Kristian <49764796+kristianka@users.noreply.github.com>
2024-06-30 01:28:29 +0300

    update metadata, add loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import React from "react";
+import React, { Suspense } from "react";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Header } from "@/components/Header";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Portfolio - Kristian Kähkönen",
-    description: "Kristian Kähkönen's portfolio. A software developer from Finland."
+    description: "Kristian Kähkönen's portfolio. A software developer from Finland.",
+    keywords: ["Kristian Kähkönen", "portfolio", "finland", "finnish", "software developer"]
 };
 
 export default function RootLayout({
@@ -26,8 +28,10 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    <Header />
-                    {children}
+                    <Suspense fallback={<Loading />}>
+                        <Header />
+                        {children}
+                    </Suspense>
                 </ThemeProvider>
             </body>
         </html>
---
+++ src/app/loading.tsx
@@ -0,0 +1,4 @@
+export default function Loading() {
+    console.log("Loading");
+    return <p>Loading...</p>;
+}
