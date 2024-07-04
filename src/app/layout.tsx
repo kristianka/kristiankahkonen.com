12ae29db1edf4d5dc70ff7bc20544e53acf4ad09
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-05 00:37:01 +0300

    improve accessability for pagespeed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html suppressHydrationWarning>
+        <html lang="en" suppressHydrationWarning>
             <body className={inter.className}>
                 <ThemeProvider
                     attribute="class"
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -58,46 +58,46 @@ export const Skills = () => {
             <div className="mt-10 sm:mt-20">
                 <h2 className="text-2xl sm:text-4xl tracking-wide font-bold m-auto">Skills</h2>
                 <div className="bg-violet-600 dark:bg-violet-700 text-white rounded-lg p-3 mt-5">
-                    <ul className="flex flex-col sm:flex-row p-1">
+                    <div className="flex flex-col sm:flex-row p-1">
                         <h3 className="text-2xl sm:text-4xl tracking-wide font-bold p-5 sm:p-10 m-auto">
                             Frontend
                         </h3>
-                        <div className="flex flex-wrap m-auto p-1">
+                        <ul className="flex flex-wrap m-auto p-1">
                             {frontendSkills.map((skill, i) => (
                                 <li key={i} className="ml-3 mr-3">
                                     {skill}
                                 </li>
                             ))}
-                        </div>
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
                 <div className="bg-violet-600 dark:bg-violet-700 text-white rounded-lg p-3 mt-5">
-                    <ul className="flex flex-col sm:flex-row p-1">
+                    <div className="flex flex-col sm:flex-row p-1">
                         <h3 className="text-2xl sm:text-4xl tracking-wide font-bold p-5 sm:p-10 m-auto">
                             Backend
                         </h3>
-                        <div className="flex flex-wrap m-auto p-1 center">
+                        <ul className="flex flex-wrap m-auto p-1 center">
                             {backendSkills.map((skill, i) => (
                                 <li key={i} className="ml-3 mr-3">
                                     {skill}
                                 </li>
                             ))}
-                        </div>
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
                 <div className="bg-violet-600 dark:bg-violet-700 text-white rounded-lg p-3 mt-5">
-                    <ul className="flex flex-col sm:flex-row p-1">
+                    <div className="flex flex-col sm:flex-row p-1">
                         <h3 className="text-2xl sm:text-4xl tracking-wide font-bold p-5 sm:p-10 m-auto">
                             DevOps
                         </h3>
-                        <div className="flex flex-wrap m-auto p-1">
+                        <ul className="flex flex-wrap m-auto p-1">
                             {devOpsSkills.map((skill, i) => (
                                 <li key={i} className="ml-3 mr-3">
                                     {skill}
                                 </li>
                             ))}
-                        </div>
-                    </ul>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
