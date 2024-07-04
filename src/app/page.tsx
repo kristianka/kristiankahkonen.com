da22b1f96998df4c3e242a42bc0b226b66294503
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-05 01:56:05 +0300

    add margin to global

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,29 +4,26 @@ import React, { Suspense } from "react";
 
 export default function Home() {
     return (
-        <main className="">
-            <div className="m-5">
-                <div className="grid grid-cols-1 sm:grid-cols-2">
-                    {/* Loading not working? */}
-                    {/* <Suspense fallback={<div>Loading...</div>}>
+        <main className="m-5">
+            <div className="grid grid-cols-1 sm:grid-cols-2">
+                {/* Loading not working? */}
+                {/* <Suspense fallback={<div>Loading...</div>}>
                         <TitleAnimation />
                     </Suspense> */}
-                    <TitleAnimation />
-                    <div className="mt-5 sm:mt-0 m-auto">
-                        I am an ICT engineering student at Tampere University of Applied Sciences
-                        (TAMK), specializing in Software Engineering and graduating May 2025. I have
-                        a strong passion for Full Stack development, focusing on creating
-                        exceptional user experiences (UX) and user interfaces (UI) with robust and
-                        safe backends.
-                        <br /> <br />
-                        My attention to detail and collaborative nature enable me to build high
-                        quality applications and work effectively within a team. I am always eager
-                        to learn new technologies and improve my skills. I am currently looking for
-                        a job as a software developer.
-                    </div>
+                <TitleAnimation />
+                <div className="mt-5 sm:mt-0 m-auto sm:text-lg">
+                    I am an ICT engineering student at Tampere University of Applied Sciences
+                    (TAMK), specializing in Software Engineering and graduating May 2025. I have a
+                    strong passion for Full Stack development, focusing on creating exceptional user
+                    experiences (UX) and user interfaces (UI) with robust and safe backends.
+                    <br /> <br />
+                    My attention to detail and collaborative nature enable me to build high quality
+                    applications and work effectively within a team. I am always eager to learn new
+                    technologies and improve my skills. I am currently looking for a job as a
+                    software developer.
                 </div>
-                <Skills />
             </div>
+            <Skills />
         </main>
     );
 }
