73e2ac16ec9b1e479b539aa452eb459834e96a88
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-14 04:35:05 +0300

    get dockerized nextjs and directurs communicating properly finally!!! :))

--- nextjs/src/services/BlogRequests.ts
+++ nextjs/src/services/BlogRequests.ts
@@ -1,22 +1,18 @@
-import axios from "axios";
-import { createDirectus, rest, readItem, authentication, readUser } from "@directus/sdk";
-import { Blog, BlogApiResponse, User } from "@/types";
+import { createDirectus, rest, readItem, authentication, readUser, readItems } from "@directus/sdk";
+import { Blog, User } from "@/types";
 
 // create a Directus client to connect to the Directus API
-const client = createDirectus("http://localhost:8055/").with(rest()).with(authentication());
+const client = createDirectus(process.env.DIRECTUS_URL as string)
+    .with(rest({ onRequest: (options) => ({ ...options, cache: "no-store" }) }))
+    .with(authentication());
 
 export const fetchBlogs = async () => {
     try {
         await client.setToken(process.env.DIRECTUS_KEY as string);
-        const res = await axios.get<BlogApiResponse>("/items/blog/", {
-            baseURL: process.env.DIRECTUS_IP,
-            headers: {
-                Authorization: `Bearer ${process.env.DIRECTUS_KEY}`
-            }
-        });
-        return res.data.data;
+        const res = (await client.request(readItems("blog"))) as Blog[];
+        return res;
     } catch (error) {
-        // console.log("Error while fetching blogs", error);
+        console.log("Error while fetching blogs", error);
         return [];
     }
 };
@@ -28,21 +24,19 @@ export const fetchBlogById = async (id: string) => {
         const result = (await client.request(readItem("blog", id))) as Blog;
         return result;
     } catch (error) {
-        // console.error("Error while fetching blog", error);
+        console.error("Error while fetching blog", error);
         return null;
     }
 };
 
 export const getBlogAuthor = async (id?: string) => {
     try {
-        console.log("get blog author");
         if (!id) return null;
         await client.setToken(process.env.DIRECTUS_KEY as string);
         const result = (await client.request(readUser(id))) as User;
-        console.log("result", result);
         return result;
     } catch (error) {
-        // console.error("Error while fetching blog author", error);
+        console.error("Error while fetching blog author", error);
         return null;
     }
 };
