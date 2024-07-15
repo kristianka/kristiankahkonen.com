463766eeb1e6359c8643f8d74c83c26a49740e49
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-15 20:47:31 +0300

    add spinners to loading pages

--- nextjs/src/app/blog/[slug]/loading.tsx
+++ nextjs/src/app/blog/[slug]/loading.tsx
@@ -1,9 +1,9 @@
 export default function Loading() {
     return (
         <main>
-            <div className="">
-                <h1 className="text-3xl sm:text-4xl text-center">Blogs</h1>
-                <h2 className="text-lg text-center text-gray-600 dark:text-gray-300">
+            <div className="flex justify-center items-center">
+                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+                <h2 className="ml-4 text-lg text-center text-gray-600 dark:text-gray-300">
                     Loading the blog...
                 </h2>
             </div>
--- nextjs/src/app/loading.tsx
+++ nextjs/src/app/loading.tsx
@@ -1,8 +1,11 @@
 export default function Loading() {
     return (
         <main>
-            <div className="">
-                <h2 className="text-center text-gray-600 dark:text-gray-300">Loading...</h2>
+            <div className="flex justify-center items-center">
+                <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-gray-900"></div>
+                <h2 className="ml-4 text-lg text-center text-gray-600 dark:text-gray-300">
+                    Loading...
+                </h2>
             </div>
         </main>
     );
