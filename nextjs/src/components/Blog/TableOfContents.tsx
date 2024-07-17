c67f57a9f796051b00d128a74d0cbc461b8561d1
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-17 21:33:09 +0300

    fix ml not working on h3

--- nextjs/src/components/Blog/TableOfContents.tsx
+++ nextjs/src/components/Blog/TableOfContents.tsx
@@ -43,19 +43,18 @@ const TableOfContents = ({ toc }: { toc: Toc[] }) => {
         <div>
             <h3 className="text-xl font-semibold uppercase">On this page</h3>
             <hr className="my-2 h-2" />
-            <ul>
+            <ul className="">
                 {toc.length === 0 && (
                     <li className="prose dark:prose-invert">No headings found.</li>
                 )}
                 {toc.map((heading, index) => {
-                    const marginLeft = heading.level - 1;
                     const isActive = heading.id === activeHeading;
 
                     return (
-                        <li key={index} className={`ml-${marginLeft * 2}`}>
+                        <li key={index}>
                             <a
                                 href={`#${heading.id}`}
-                                className={`${
+                                className={`ml-${heading.level} ${
                                     isActive
                                         ? "prose dark:prose-invert text-blue-600 transition-all font-bold"
                                         : "prose dark:prose-invert font-semibold transition-all hover:text-black dark:hover:text-white"
