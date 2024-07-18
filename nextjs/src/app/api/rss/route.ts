1b3e10dc2ce9a9e44fb2f8d39dd54a7174fde2a4
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-19 01:41:39 +0300

    remove hardcode url and email

--- nextjs/src/app/api/rss/route.ts
+++ nextjs/src/app/api/rss/route.ts
@@ -4,32 +4,35 @@ import { Feed } from "feed";
 import { Blog } from "@/types";
 
 const generateRSSFeed = (blogs: Blog[]) => {
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL as string;
+    const email = process.env.NEXT_PUBLIC_EMAIL as string;
+
     const feed = new Feed({
         title: "Kristian Kähkönen",
         description: "Blog posts about web development, software engineering, and more.",
-        id: "https://kristiankahkonen.com/",
-        link: "https://kristiankahkonen.com/",
-        favicon: `https://kristiankahkonen.com/favicon.ico`,
+        id: siteUrl,
+        link: siteUrl,
+        favicon: `${siteUrl}/favicon.ico`,
         language: "en",
         updated: new Date(),
         generator: "Next.js using Feed for RSS",
         copyright: "© 2024 Kristian Kähkönen. All rights reserved.",
         feedLinks: {
-            rss: "https://kristiankahkonen.com/rss.xml"
+            rss: `${siteUrl}/rss.xml`
         }
     });
 
     blogs.forEach((blog) => {
         feed.addItem({
             title: blog.title,
-            id: `https://kristiankahkonen.com/blog/${blog.id}`,
-            link: `https://kristiankahkonen.com/blog/${blog.id}`,
+            id: `${siteUrl}/blog/${blog.id}`,
+            link: `${siteUrl}/blog/${blog.id}`,
             description: blog.description,
             author: [
                 {
                     name: "Kristian Kähkönen",
-                    email: "kristian@kristiankahkonen.com",
-                    link: "https://kristiankahkonen.com"
+                    email: email,
+                    link: siteUrl
                 }
             ],
             date: new Date(blog.date_created)
