c2233e40fa09c20fa568f5972bd3a7047f91c7b4
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-29 18:21:48 +0300

    add share button

--- nextjs/src/components/Blog/BlogPage.tsx
+++ nextjs/src/components/Blog/BlogPage.tsx
@@ -10,8 +10,11 @@ import { Blog, Toc, User } from "@/types";
 // smooth scroll to anchor links
 import "../../styles.css";
 import { TableOfContentsMobile } from "./TableOfContentsMobile";
+import { ShareButton } from "../ShareButton";
 
 export const BlogPage = ({ blog, user, toc }: { blog: Blog; user: User | null; toc: Toc[] }) => {
+    const blogUrl = `${process.env.NEXT_PUBLIC_SITE_URL}/blog/${blog.id}`;
+
     return (
         <div key={blog.id} className="">
             <div>
@@ -19,19 +22,24 @@ export const BlogPage = ({ blog, user, toc }: { blog: Blog; user: User | null; t
                 <h3 className="sm:text-xl mb-3 prose dark:prose-invert italic ">
                     {blog.description}
                 </h3>
-                {blog && user && (
-                    <h4 className="sm:text-lg prose dark:prose-invert">
-                        Author: {user.first_name} {user.last_name}
-                    </h4>
-                )}
-                <h4 className="prose dark:prose-invert">
-                    <DateToLocal date={blog.date_created} type="published" />
-                </h4>
-                {blog.date_updated && (
+                <div>
+                    <div className="flex">
+                        {blog && user && (
+                            <h4 className="sm:text-lg prose dark:prose-invert">
+                                Author: {user.first_name} {user.last_name}
+                            </h4>
+                        )}
+                        <ShareButton title={blog.title} text={blog.description} url={blogUrl} />
+                    </div>
                     <h4 className="prose dark:prose-invert">
-                        <DateToLocal date={blog.date_updated} type="updated" />
+                        <DateToLocal date={blog.date_created} type="published" />
                     </h4>
-                )}
+                    {blog.date_updated && (
+                        <h4 className="prose dark:prose-invert">
+                            <DateToLocal date={blog.date_updated} type="updated" />
+                        </h4>
+                    )}
+                </div>
                 <div className="block sm:hidden">
                     <TableOfContentsMobile toc={toc} />
                 </div>
