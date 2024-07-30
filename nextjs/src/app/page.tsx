4e0931a41aa30f98b4eb46272dcd06a81687e35c
Kristian <49764796+kristianka@users.noreply.github.com>
2024-07-31 02:12:10 +0300

    add dark theme, change order, fix overflow on mobile

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -11,28 +11,28 @@ export default async function Home() {
     return (
         <main className="">
             <TitleAnimation />
-            <div className="mt-10 space-x-3">
+            <div className="mt-10 flex flex-wrap  justify-start gap-3">
                 <Link
                     href="/blog"
-                    className="hover:bg-blue-500 border border-black px-10 py-2 uppercase rounded-full bg-white"
+                    className="hover:bg-blue-500 border border-black px-4 py-2 sm:px-10 sm:py-2 uppercase rounded-full bg-white dark:bg-black dark:text-white dark:border-white dark:hover:text-blue-500"
                 >
                     Blog
                 </Link>
                 <Link
-                    href="/about"
-                    className="hover:text-blue-500 border border-white px-10 py-2 uppercase rounded-full bg-black text-white"
-                >
-                    About
-                </Link>
-                <Link
                     href="/projects"
-                    className="hover:bg-blue-500 border border-black px-10 py-2 uppercase rounded-full bg-white"
+                    className="hover:text-blue-500 border border-white px-4 py-2 sm:px-10 sm:py-2 uppercase rounded-full bg-black text-white dark:bg-white dark:text-black dark:border-black dark:hover:bg-blue-500"
                 >
                     Projects
                 </Link>
                 <Link
+                    href="/about"
+                    className="hover:bg-blue-500 border border-black px-4 py-2 sm:px-10 sm:py-2 uppercase rounded-full bg-white dark:bg-black dark:text-white dark:border-white dark:hover:text-blue-500"
+                >
+                    About
+                </Link>
+                <Link
                     href="/contact"
-                    className="hover:text-blue-500 border border-white px-10 py-2 uppercase rounded-full bg-black text-white"
+                    className="hover:text-blue-500 border border-white px-4 py-2 sm:px-10 sm:py-2 uppercase rounded-full bg-black text-white dark:bg-white dark:text-black dark:border-black dark:hover:bg-blue-500"
                 >
                     Contact
                 </Link>
