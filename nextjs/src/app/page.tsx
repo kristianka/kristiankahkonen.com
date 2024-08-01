fd752f0e9bccd57944c542a24d4464000ecdbc12
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-01 18:45:56 +0300

    add blogs to frontpage

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TitleAnimation } from "@/components/TitleAnimation";
 import FrontpageButtons from "@/components/Frontpage/FrontpageButtons";
+import FrontpageBlogs from "@/components/Frontpage/FrontpageBlogs";
 
 export default async function Home() {
     return (
         <main>
             <TitleAnimation />
             <FrontpageButtons />
+            <FrontpageBlogs />
         </main>
     );
 }
