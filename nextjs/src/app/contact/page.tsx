8c6fddbf39da02e79a8fd6be5ba6e39824ad9446
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-01 18:42:29 +0300

    update contact and projects page

--- nextjs/src/app/contact/page.tsx
+++ nextjs/src/app/contact/page.tsx
@@ -1,11 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Contact } from "@/components/Contact";
 
 export default function Home() {
     return (
-        <main className="flex">
-            <div className="m-auto mt-5 text-center min-h-screen">
-                <Contact />
+        <main className="min-h-screen">
+            <div>
+                <h2 className="text-2xl sm:text-4xl tracking-wide font-bold m-auto mb-5">
+                    Contact
+                </h2>
             </div>
+            <h3 className="text-lg">
+                You can contact me via email at the address below. I'll be in touch ASAP!{" "}
+            </h3>
+            <Contact />
         </main>
     );
 }
