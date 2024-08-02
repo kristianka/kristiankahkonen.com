6ab4ed0125182ada270041e81e4f46d5a8aabbc3
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-02 15:57:21 +0300

    fix icon alignment on mobile

--- nextjs/src/components/Contact.tsx
+++ nextjs/src/components/Contact.tsx
@@ -5,14 +5,12 @@ export const Contact = () => {
         <div className="mt-10 sm:mt-20 ">
             <a className="" href="mailto: kristian@kristiankahkonen.com">
                 <div className="bg-yellow-300 p-5 rounded-lg flex flex-wrap items-center space-x-5">
-                    <Mail className="dark:text-black" size={48} />
+                    <Mail className="dark:text-black" size={32} />
                     <div>
-                        <h3 className=" text-xl sm:text-2xl font-bold dark:text-black">
+                        <h3 className="text-xl sm:text-2xl font-bold dark:text-black">
                             Kristian Kähkönen
                         </h3>
-                        <p className="text-lg sm:text-xl dark:text-black">
-                            kristian@kristiankahkonen.com
-                        </p>
+                        <p className="sm:text-xl dark:text-black">kristian@kristiankahkonen.com</p>
                     </div>
                 </div>
             </a>
