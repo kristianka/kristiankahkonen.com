812ebc70f9079c893b8c9b03dc71d8b302b774f0
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-10 00:54:13 +0300

    projects are now fetched from CMS, not hardcoded

--- nextjs/src/app/projects/page.tsx
+++ nextjs/src/app/projects/page.tsx
@@ -1,60 +1,34 @@
 import TextParallaxContent from "@/components/Projects/TextParallaxContext";
 import Content from "@/components/Projects/Content";
+import { getProjects } from "@/services/BlogRequests";
 
-export default function Home() {
-    const pc = process.env.NEXT_PUBLIC_TEMP_IMG_URL_PC as string;
-    const mobile = process.env.NEXT_PUBLIC_TEMP_IMG_URL_MOBILE as string;
+export default async function Home() {
+    const projects = await getProjects();
+    console.log(projects);
 
     return (
         <div className="">
-            <TextParallaxContent
-                imgUrlPc={pc}
-                imgUrlMobile={mobile}
-                subheading="MYMEMORIA.APP"
-                heading="A digital memory box."
-            >
-                <Content
-                    title="Saving memories with MyMemoria"
-                    description="MyMemoria is a fullstack web application that helps you remembering important
-                    events. You can create, update, and delete memories. This was my FullstackOpen
-                    project that took over 250 hours to make."
-                    techStack={["React", "Node.js", "Express", "MongoDB"]}
-                    sourceCodeUrl=""
-                    liveUrl=""
-                />
-            </TextParallaxContent>
-            <TextParallaxContent
-                imgUrlPc={pc}
-                imgUrlMobile={mobile}
-                subheading="BoxDB app"
-                heading="Logistics sidekick."
-            >
-                <Content
-                    title="Saving memories with MyMemoria"
-                    description="MyMemoria is a fullstack web application that helps you remembering important
-                    events. You can create, update, and delete memories. This was my FullstackOpen
-                    project that took over 250 hours to make."
-                    techStack={["React", "Node.js", "Express", "MongoDB"]}
-                    sourceCodeUrl=""
-                    liveUrl=""
-                />
-            </TextParallaxContent>
-            <TextParallaxContent
-                imgUrlPc={pc}
-                imgUrlMobile={mobile}
-                subheading="Timetable app"
-                heading="Time management."
-            >
-                <Content
-                    title="Saving memories with MyMemoria"
-                    description="MyMemoria is a fullstack web application that helps you remembering important
-                    events. You can create, update, and delete memories. This was my FullstackOpen
-                    project that took over 250 hours to make."
-                    techStack={["React", "Node.js", "Express", "MongoDB"]}
-                    sourceCodeUrl=""
-                    liveUrl=""
-                />
-            </TextParallaxContent>
+            {projects.length === 0 && (
+                <h1 className="mx-auto text-2xl text-center">No projects found 😞</h1>
+            )}
+            {projects &&
+                projects.map((project) => (
+                    <TextParallaxContent
+                        key={project.id}
+                        imgUrlPc={project.image.imgUrlPc}
+                        imgUrlMobile={project.image.imgUrlMobile}
+                        subheading={project.image.title}
+                        heading={project.image.subtitle}
+                    >
+                        <Content
+                            title={project.content.title}
+                            description={project.content.description}
+                            techStack={project.content.techStack}
+                            sourceCodeUrl={project.content.sourceCodeUrl}
+                            liveUrl={project.content.liveUrl}
+                        />
+                    </TextParallaxContent>
+                ))}
         </div>
     );
 }
--- nextjs/src/services/BlogRequests.ts
+++ nextjs/src/services/BlogRequests.ts
@@ -19,7 +19,6 @@ export const fetchBlogs = async () => {
             );
         return sortedBlogs;
     } catch (error) {
-        console.error("Error while fetching blogs", error);
         return [];
     }
 };
@@ -31,7 +30,6 @@ export const fetchBlogById = async (id: string) => {
         if (!res.published) return null;
         return res;
     } catch (error) {
-        console.error("Error while fetching blog", error);
         return null;
     }
 };
@@ -42,7 +40,6 @@ export const getBlogAuthor = async (id?: string) => {
         const res = (await client.request(readUser(id))) as User;
         return res;
     } catch (error) {
-        console.error("Error while fetching blog author", error);
         return null;
     }
 };
@@ -58,7 +55,6 @@ export const getFeaturedBlogs = async () => {
             .slice(0, 3);
         return featuredBlogs;
     } catch (error) {
-        console.error("Error while fetching featured blogs", error);
         return [];
     }
 };
@@ -73,7 +69,47 @@ export const getLatestBlog = async () => {
             .slice(0, 1);
         return latestBlog;
     } catch (error) {
-        console.error("Error while fetching latest blog", error);
+        return [];
+    }
+};
+
+interface Project {
+    status: "published" | "draft";
+    id: string;
+    order: number;
+    date_created: string;
+    published: boolean;
+    image: Image;
+    content: Content;
+}
+
+interface Image {
+    id: string;
+    title: string;
+    subtitle: string;
+    imgUrlPc: string;
+    imgUrlMobile: string;
+}
+
+interface Content {
+    id: string;
+    title: string;
+    description: string;
+    techStack: string[];
+    sourceCodeUrl: string;
+    liveUrl: string;
+}
+
+export const getProjects = async () => {
+    try {
+        const res = (await client.request(
+            readItems("project", {
+                fields: ["*", { image: ["*"], content: ["*"] }]
+            })
+        )) as Project[];
+        const sortedProjects = res.filter((project) => project.status === "published");
+        return sortedProjects;
+    } catch (error) {
         return [];
     }
 };
