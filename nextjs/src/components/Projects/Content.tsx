36cdf07b40548b8b86e2e2a99f87689c92f6632e
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-10 01:35:31 +0300

    add dark theme for text, add links to source code and live urls

--- nextjs/src/components/Projects/Content.tsx
+++ nextjs/src/components/Projects/Content.tsx
@@ -1,4 +1,8 @@
 import { FiArrowUpRight } from "react-icons/fi";
+import { FaGithub } from "react-icons/fa";
+import { CiGlobe } from "react-icons/ci";
+
+import Link from "next/link";
 
 interface ContentProps {
     title: string;
@@ -6,6 +10,7 @@ interface ContentProps {
     techStack: string[];
     sourceCodeUrl: string;
     liveUrl?: string;
+    liveUrlShort?: string;
 }
 
 export default function Content({
@@ -13,24 +18,44 @@ export default function Content({
     description,
     techStack,
     sourceCodeUrl,
-    liveUrl
+    liveUrl,
+    liveUrlShort
 }: ContentProps) {
+    const hasLiveUrl = liveUrl !== null && liveUrlShort !== null;
     return (
         <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 px-4 pb-24 pt-12 md:grid-cols-12">
             <h2 className="col-span-1 text-3xl font-bold md:col-span-4">{title}</h2>
             <div className="col-span-1 md:col-span-8">
-                <p className="mb-4 text-xl text-neutral-600 md:text-2xl">{description}</p>
-                <p className="mb-8 text-xl text-neutral-600 md:text-2xl">{techStack}</p>
-                {sourceCodeUrl && (
-                    <button className="w-full rounded bg-neutral-900 px-9 py-4 text-xl text-white transition-colors hover:bg-neutral-700 md:w-fit">
-                        Source code <FiArrowUpRight className="inline" />
-                    </button>
-                )}
-                {liveUrl && (
-                    <button className="md:ml-5 mt-5 md:mt-0 w-full rounded bg-neutral-900 px-9 py-4 text-xl text-white transition-colors hover:bg-neutral-700 md:w-fit">
-                        Live at <FiArrowUpRight className="inline" />
-                    </button>
-                )}
+                <p className="mb-4 text-xl text-neutral-600 dark:text-neutral-300 md:text-2xl">
+                    {description}
+                </p>
+                <p className="mb-8 text-xl text-neutral-600 dark:text-neutral-300 md:text-2xl">
+                    {techStack}
+                </p>
+                <div className="flex sm:flex-row flex-col">
+                    {sourceCodeUrl && (
+                        <Link
+                            href={sourceCodeUrl}
+                            target="_blank"
+                            className="flex items-center rounded bg-neutral-900 px-8 py-4 text-xl text-white transition-colors hover:bg-neutral-700 "
+                        >
+                            <FaGithub className="mr-3 w-6 h-6" />
+                            Source code
+                            <FiArrowUpRight className="inline" />
+                        </Link>
+                    )}
+                    {hasLiveUrl && (
+                        <Link
+                            href={liveUrl as string}
+                            target="_blank"
+                            className="sm:ml-5 mt-5 sm:mt-0 flex items-center rounded bg-neutral-900 px-8 py-4 text-xl text-white transition-colors hover:bg-neutral-700 "
+                        >
+                            <CiGlobe className="mr-3 w-6 h-6" />
+                            Live at {liveUrlShort}
+                            <FiArrowUpRight className="inline" />
+                        </Link>
+                    )}
+                </div>
             </div>
         </div>
     );
