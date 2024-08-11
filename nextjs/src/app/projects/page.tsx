dcbb879c4556803d5b9345b5b220bd391159ac86
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-12 00:38:30 +0300

    add more projects, make disclaimer own component

--- nextjs/src/app/projects/page.tsx
+++ nextjs/src/app/projects/page.tsx
@@ -1,6 +1,8 @@
 import TextParallaxContent from "@/components/Projects/TextParallaxContext";
 import Content from "@/components/Projects/Content";
 import { getProjects } from "@/services/BlogRequests";
+import MoreProjects from "@/components/Projects/MoreProjects";
+import Disclaimer from "@/components/Projects/Disclaimer";
 
 export default async function Home() {
     const projects = await getProjects();
@@ -30,12 +32,8 @@ export default async function Home() {
                         />
                     </TextParallaxContent>
                 ))}
-            <p className="-mb-44 text-xs sm:text-sm my-32 text-neutral-600 dark:text-neutral-300 text-center sm:max-w-5xl sm:mx-auto mx-5">
-                All logos and trademarks displayed on this page are the property of their respective
-                owners. The use of these logos is purely for informational purposes, to identify the
-                technologies used in these projects. The use of these logos does not imply any
-                endorsement or affiliation.
-            </p>
+            <MoreProjects />
+            <Disclaimer />
         </div>
     );
 }
