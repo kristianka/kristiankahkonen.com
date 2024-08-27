da784ecee8ea6e194be6ae3858e99d537605c029
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-28 02:15:15 +0300

    change education layout, get data from CMS and not hardcoded

--- nextjs/src/components/About/Education.tsx
+++ nextjs/src/components/About/Education.tsx
@@ -1,31 +1,27 @@
+import { getEducations } from "@/services/BlogRequests";
 import EducationCard from "./EducationCard";
 
-export default function Education() {
+export default async function Education() {
+    const educations = await getEducations();
     return (
         <div>
             <h2 className="text-2xl sm:text-4xl tracking-wide font-bold m-auto mb-5">
                 Education and Certifications
             </h2>
-            <EducationCard
-                title="Tampere University of Applied Sciences (TAMK)"
-                date="August 2021 - May 2025"
-                location="Tampere, Finland"
-                description="I learned a lot of about web development from University of Helsinki courses, like FullstackOpen. I have done it fully including the project, totaling 24 ECTS. I have also done the DevOps with Docker course. I have also done other courses from other universities, like Japanese language courses from Aalto University."
-                gpa="GPA: 4,14 / 5"
-            />
-            <EducationCard
-                title="Vocational Qualification in Information and Communication Technology"
-                date="August 2018 - May 2021"
-                location="Hämeenlinna, Finland"
-                description="GPA: 4,83"
-                gpa="GPA: 4,83 / 5"
-            />
-            <p className="text-gray-500 mb-3">
-                I have completed several university courses, including the University of
-                Helsinki&apos;s Full Stack Open course and project, for which I received great
-                feedback. You can read the feedback here. I also completed their DevOps with Docker
-                course. Additionally, I have taken Japanese 1 and 2 courses at Aalto University.
-            </p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+                {educations &&
+                    educations.map((education) => (
+                        <EducationCard
+                            key={education.id}
+                            title={education.title}
+                            school={education.school}
+                            date={education.date}
+                            location={education.location}
+                            description={education.description}
+                            gpa={education.gpa}
+                        />
+                    ))}
+            </div>
         </div>
     );
 }
--- nextjs/src/components/About/EducationCard.tsx
+++ nextjs/src/components/About/EducationCard.tsx
@@ -2,6 +2,7 @@ interface EducationCardProps {
     title: string;
     date: string;
     location: string;
+    school: string;
     description: string;
     gpa: string;
 }
@@ -9,17 +10,19 @@ interface EducationCardProps {
 export default function EducationCard({
     title,
     date,
+    school,
     location,
     description,
     gpa
 }: EducationCardProps) {
     return (
-        <div className="my-3">
+        <div className="mb-5">
             <h3 className="text-xl font-bold">{title}</h3>
-            <p className="text-gray-600 text-sm">{date}</p>
-            <p className="text-gray-600 text-sm">{location}</p>
-            {/* <p className="text-gray-600 text-sm">{description}</p> */}
-            <p className="">{gpa}</p>
+            <h4 className="prose dark:prose-invert">{school}</h4>
+            <p className="prose dark:prose-invert">{location}</p>
+            <p className="prose dark:prose-invert">{date}</p>
+            <p className="prose dark:prose-invert font-bold">{gpa}</p>
+            <p className="prose dark:prose-invert">{description}</p>
         </div>
     );
 }
