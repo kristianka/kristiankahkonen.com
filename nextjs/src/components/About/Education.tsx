6e5de903ead61ae7d7c5a87a0f2663a962963fa5
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-29 02:35:04 +0300

    change margins

--- nextjs/src/components/About/Education.tsx
+++ nextjs/src/components/About/Education.tsx
@@ -5,10 +5,10 @@ export default async function Education() {
     const educations = await getEducations();
     return (
         <div>
-            <h2 className="text-2xl sm:text-4xl tracking-wide font-bold m-auto mb-5">
+            <h2 className="text-2xl sm:text-4xl tracking-wide font-bold m-auto mb-10">
                 Education and Certifications
             </h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20">
                 {educations &&
                     educations.map((education) => (
                         <EducationCard
