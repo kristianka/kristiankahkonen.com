6e537cd10707c2ce4f04428c60613229b32a25dc
Kristian <49764796+kristianka@users.noreply.github.com>
2024-08-31 19:52:45 +0300

    make empty loading to About and Contact, change fadein speed to be faster

--- nextjs/src/app/contact/page.tsx
+++ nextjs/src/app/contact/page.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable react/no-unescaped-entities */
 import { Contact } from "@/components/Contact";
+import FadeIn from "@/components/FadeIn";
 
 export default function Home() {
     return (
         <main>
-            <h2 className="text-center text-2xl sm:text-4xl tracking-wide font-bold m-auto mb-5">
-                Contact
-            </h2>
-            <Contact />
+            <FadeIn>
+                <h2 className="text-center text-2xl sm:text-4xl tracking-wide font-bold m-auto mb-5">
+                    Contact
+                </h2>
+                <Contact />
+            </FadeIn>
         </main>
     );
 }
--- nextjs/src/components/FadeIn.tsx
+++ nextjs/src/components/FadeIn.tsx
@@ -10,7 +10,7 @@ export default function FadeIn({ children }: FadeInProps) {
         <motion.div
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.6, ease: "easeOut" }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
             viewport={{ once: true }}
         >
             {children}
